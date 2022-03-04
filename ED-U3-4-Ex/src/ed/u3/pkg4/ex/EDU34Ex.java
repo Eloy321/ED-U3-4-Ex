5b69861832feb21cef86f6561a14850939fa201a
elolop <elolop@ubuntu>
2022-03-04 16:04:49 +0100

    He creados tres métodos uno de suma de enteros otro de suma de decimales y otro de división de decimales.Y he creado la documentación.

--- ED-U3-4-Ex/src/ed/u3/pkg4/ex/EDU34Ex.java
+++ ED-U3-4-Ex/src/ed/u3/pkg4/ex/EDU34Ex.java
@@ -1,20 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ed.u3.pkg4.ex;
 
 /**
+ * La clase EDU34Ex su funcion es sumar.
  *
- * @author elolop
+ * @author Eloy Beltrán López
  */
 public class EDU34Ex {
 
     /**
-     * @param args the command line arguments
+     * @param args Comando de línea de argumentos.
+     */
+    /**
+     * Metodo suma: Suma dos números enteros a y b.
+     *
+     * @param a Primer numero entero que va a sumar.
+     * @param b Segundo numero entero que va a sumar.
+     * @return Resultado de la suma (a+b).
+     */
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Metodo suma: Suma dos números decimales a y b.
+     *
+     * @param a Primer numero decimal que va a sumar.
+     * @param b Segundo numero decimal que va a sumar.
+     * @return Resultado de la suma (a+b).
+     */
+    public static double suma(double a, double b) {
+        return a + b;
+    }
+
+    /**
+     * Metodo divide: Divide dos números decimales a y b.
+     *
+     * @param a Primer numero decimal que va a dividir.
+     * @param b Segundo numero decimal que va a sumar.
+     * @return Resultado de la división (a/b)
+     */
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("División por cero!");
+        }
+        return a / b;
+    }
+
+    /**
+     * Metodo main
+     *
+     * @param args
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        
     }
-    
+
 }

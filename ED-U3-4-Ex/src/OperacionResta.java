65f80fa3500b9286474a46893abef05cb72d7335
elolop <elolop@ubuntu>
2022-03-04 16:19:44 +0100

    He creado una nueva clase llamada opercaión resta que tiene dos métodos los cuales su función es hacer restas.

---
+++ ED-U3-4-Ex/src/OperacionResta.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ * La clase OperacionResta tiene la funcion de restar.
+ *
+ * @author Eloy Beltrán López
+ */
+public class OperacionResta {
+
+    /**
+     * Metodo resta: resta dos números enteros 'x' e 'y'
+     *
+     * @param x Primer numero entero de la resta.
+     * @param y Segundo nuemro entero de la resta.
+     * @return Resultado de la resta (x-y).
+     */
+    public static int resta(int x, int y) {
+        return x - y;
+    }
+
+    /**
+     * Metodo resta: resta dos números decimales 'x' e 'y'
+     *
+     * @param x Primer numero decimal de la resta.
+     * @param y Segundo nuemro decimal de la resta.
+     * @return Resultado de la resta (x-y).
+     */
+    public static double resta(double x, double y) {
+        return x - y;
+    }
+
+}
